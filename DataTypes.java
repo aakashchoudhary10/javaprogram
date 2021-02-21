8282585ad717d7e94ede788962c0f63ee60292c7
Aakash Choudhary <aakashchoudhary@DLLA1660.qait.com>
2021-02-21 15:40:25 +0530

    first commit

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+package basicJave;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		//Primitive Data Types:- int,double char boolean
+		
+		//1. Interger:- int
+		int i = 10;
+		//int i=20; duplicate data types not allowed
+		i=20; //We can update data types value 
+		
+	System.out.println(i);
+	System.out.println(i++);
+	System.out.println(i);
+	System.out.println(++i);
+	System.out.println(i);
+		
+	}
+
+}
